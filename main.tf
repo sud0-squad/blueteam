6f8f7013a20d3620ea59273619dd225ed6fdb9fc
danielwchas <daniel.wagenius@chasacademy.se>
2026-09-14 11:01:48 +0200

    Lagt till Service Account för Daniel 2

--- main.tf
+++ main.tf
@@ -91,10 +91,10 @@ resource "google_compute_instance" "jumphost" {
       size  = 20
     }
   }
-  
+
   service_account {
-  email  = "team${var.team_id}-jumphost@${var.project_id}.iam.gserviceaccount.com"
-  scopes = ["cloud-platform"]
+    email  = "team${var.team_id}-jumphost@${var.project_id}.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
   }
 
   network_interface {
